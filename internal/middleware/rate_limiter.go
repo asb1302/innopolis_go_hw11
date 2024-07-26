f528f56891ee0c8b1556b6c690052d5aeb7b35a1
Alexey B <bas95work@gmail.com>
2024-07-26 19:17:51 +0300

    refactor

--- internal/middleware/rate_limiter.go
+++ internal/middleware/rate_limiter.go
@@ -31,41 +31,42 @@ func RateLimiterMiddleware(cfg config.RateLimiterConfig) func(http.Handler) http
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			ip := getIP(r)
-			log.Printf("Запрос от IP: %s", ip)
+			log.Printf("Входящий запрос от IP: %s", ip)
 
 			mu.Lock()
 			defer mu.Unlock()
 
 			client, exists := clients[ip]
 			if !exists {
-				log.Printf("Новый клиент: %s", ip)
-
+				// Новый клиент
+				log.Printf("Обнаружен новый клиент: %s", ip)
 				clients[ip] = &Client{requests: 1, lastSeen: time.Now()}
-			} else {
-				now := time.Now()
-				if now.Sub(client.lastSeen) > cfg.Duration {
-					log.Printf("Сброс счетчика запросов для клиента %s из-за превышения длительности", ip)
-
-					client.requests = 1
-					client.lastSeen = now
-				} else {
-					client.requests++
-
-					if client.requests > cfg.Requests {
-						log.Printf("Клиент %s превысил лимит запросов", ip)
-
-						w.WriteHeader(http.StatusTooManyRequests)
-						w.Write([]byte("Слишком много запросов"))
-						return
-					}
-				}
+				next.ServeHTTP(w, r)
+				return
 			}
 
-			if client != nil {
-				log.Printf("Счетчик запросов клиента %s: %d, lastSeen: %s", ip, client.requests, client.lastSeen.Format(time.RFC3339))
-			} else {
-				log.Printf("Клиент %s пока не инициализирован", ip)
+			// Существующий клиент
+			now := time.Now()
+			if now.Sub(client.lastSeen) > cfg.Duration {
+				// Прошло больше времени, чем указано в cfg.Duration
+				log.Printf("Сброс счетчика запросов для клиента %s из-за превышения длительности", ip)
+				client.requests = 1
+				client.lastSeen = now
+				next.ServeHTTP(w, r)
+
+				return
 			}
+
+			client.requests++
+			if client.requests > cfg.Requests {
+				// Превышен лимит запросов
+				log.Printf("Клиент %s превысил лимит запросов", ip)
+				w.WriteHeader(http.StatusTooManyRequests)
+				w.Write([]byte("Слишком много запросов"))
+				return
+			}
+
+			log.Printf("Счетчик запросов клиента %s: %d, lastSeen: %s", ip, client.requests, client.lastSeen.Format(time.RFC3339))
 
 			next.ServeHTTP(w, r)
 		})
